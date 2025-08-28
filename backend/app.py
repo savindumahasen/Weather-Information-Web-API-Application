689273e83fd6ab2188350a67f1c4421eaf1d5853
Savindu <savinduruhunuhewa@gmail.com>
2025-08-28 10:06:31 +0530

    Add the cities code value to  array

--- backend/app.py
+++ backend/app.py
@@ -15,16 +15,19 @@ if weather_API_key:
 else:
     print("Api key is not loaded successfully")
 
+cityCodesArray = []
 
 with open('C:\\Users\\THIS PC\\Desktop\\fidenztechnologies(Assignment)\\Weather-Information-Web-API-Application\\backend\\cities.json','r') as file:
     data=json.load(file)
     for city_ids in data['List']:
         city_codes=city_ids['CityCode']
         print(city_codes)
+        cityCodesArray.append(city_codes)
+print(cityCodesArray)
      
 
 
 
 
 if __name__=='__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
