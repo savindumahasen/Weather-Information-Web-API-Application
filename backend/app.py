37e7836b5dcbd5a3c41e296c9455c2a3f23a23dc
Savindu <savinduruhunuhewa@gmail.com>
2025-08-28 10:47:03 +0530

    Create the route called weather

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask,jsonify
 from dotenv import load_dotenv
 import json
 
@@ -17,13 +17,28 @@ else:
 
 cityCodesArray = []
 
-with open('C:\\Users\\THIS PC\\Desktop\\fidenztechnologies(Assignment)\\Weather-Information-Web-API-Application\\backend\\cities.json','r') as file:
-    data=json.load(file)
-    for city_ids in data['List']:
-        city_codes=city_ids['CityCode']
-        print(city_codes)
-        cityCodesArray.append(city_codes)
-print(cityCodesArray)
+def  extractCityCode():
+    with open('C:\\Users\\THIS PC\\Desktop\\fidenztechnologies(Assignment)\\Weather-Information-Web-API-Application\\backend\\cities.json','r') as file:
+        data=json.load(file)
+        for city_ids in data['List']:
+            city_codes=city_ids['CityCode']
+            print(city_codes)
+            cityCodesArray.append(city_codes)
+    print(cityCodesArray)
+    return cityCodesArray
+
+
+@app.route("/weather", methods=["GET"])
+def retrieve_weather_data():
+    cityCodes=extractCityCode()
+    print(cityCodes)
+
+    if not cityCodes:
+        return jsonify("cityCodes  parameter is empty")
+    else:
+        return jsonify("citycodes parameter is not empty")
+      
+
      
 
 
