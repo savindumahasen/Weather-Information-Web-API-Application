1a1a3380a0ac8292b8a50c3fbead546e1301e204
Savindu <savinduruhunuhewa@gmail.com>
2025-08-28 09:04:11 +0530

    Load the APi key from .env file

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,17 @@ from dotenv import load_dotenv
 
 
 app=Flask(__name__)
+load_dotenv()
+
+
+weather_API_key = os.getenv("weatherAPIKey")
+
+if weather_API_key:
+    print("API key is loaded successfully")
+else:
+    print("Api key is not loaded successfully")
+
+
 
 
 
