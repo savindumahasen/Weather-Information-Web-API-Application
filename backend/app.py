dced368278df3b59479f2b42ab1a60043a50e06e
Savindu <savinduruhunuhewa@gmail.com>
2025-08-28 12:20:23 +0530

    Remove the unnecessary comments

--- backend/app.py
+++ backend/app.py
@@ -19,46 +19,47 @@ app.config.from_mapping(config)
 catche=Cache(app)
 
 
-#weather_API_key = os.getenv("weatherAPIKey")
+weather_API_key = os.getenv("weatherAPIKey")
 
-#if weather_API_key:
-#    print("API key is loaded successfully")
-#else:
-#    print("Api key is not loaded successfully")
+if weather_API_key:
+    print("API key is loaded successfully")
+else:
+    print("Api key is not loaded successfully")
 
-#cityCodesArray = []
+cityCodesArray = []
 
-#def  extractCityCode():
-#    with open('C:\\Users\\THIS PC\\Desktop\\fidenztechnologies(Assignment)\\Weather-Information-Web-API-Application\\backend\\cities.json','r') as file:
-#        data=json.load(file)
-#        for city_ids in data['List']:
-#            city_codes=city_ids['CityCode']
-#            print(city_codes)
-#            cityCodesArray.append(city_codes)
-#    print(cityCodesArray)
-#    return cityCodesArray
+#@cache.cached(timeout=300, key_prefix='weather_data')
+def  extractCityCode():
+    with open('C:\\Users\\THIS PC\\Desktop\\fidenztechnologies(Assignment)\\Weather-Information-Web-API-Application\\backend\\cities.json','r') as file:
+        data=json.load(file)
+        for city_ids in data['List']:
+            city_codes=city_ids['CityCode']
+            print(city_codes)
+            cityCodesArray.append(city_codes)
+    print(cityCodesArray)
+    return cityCodesArray
 
 
-#@app.route("/weather", methods=["GET"])
-#def retrieve_weather_data():
-#    cityCodes=extractCityCode()
-#    print(cityCodes)
+@app.route("/weather", methods=["GET"])
+def retrieve_weather_data():
+    cityCodes=extractCityCode()
+    print(cityCodes)
 
-#    if not cityCodes:
-#        return jsonify("cityCodes  parameter is empty")
-#    else:
-#     try:
-#        weather_data_array=[]
-#        for city_id in cityCodes:
-#            url=f"https://api.openweathermap.org/data/2.5/weather?id={city_id}&units=metric&appid={weather_API_key}"
-#            response=requests.get(url)
-#            weather_data=response.json()
-#            print(weather_data)
-#            weather_data_array.append(weather_data)
-#        print(weather_data_array)
-#        return jsonify(weather_data_array)   
-#     except:
-#         return jsonify("Failing to  fetch the data")
+    if not cityCodes:
+        return jsonify("cityCodes  parameter is empty")
+    else:
+     try:
+        weather_data_array=[]
+        for city_id in cityCodes:
+            url=f"https://api.openweathermap.org/data/2.5/weather?id={city_id}&units=metric&appid={weather_API_key}"
+            response=requests.get(url)
+            weather_data=response.json()
+            print(weather_data)
+            weather_data_array.append(weather_data)
+        print(weather_data_array)
+        return jsonify(weather_data_array)   
+     except:
+         return jsonify("Failing to  fetch the data")
         
 
 
