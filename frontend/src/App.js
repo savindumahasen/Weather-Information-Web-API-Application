9cbfab076495e8af3039c3483b0827adffeeaa0d
Savindu <savinduruhunuhewa@gmail.com>
2025-08-29 22:35:40 +0530

    Add the commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,22 @@ import './App.css';
 import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
+import Dashboard from './pages/dashboard';
+import Testdashboard from  './pages/testdashbord'
+import TestUser from './pages/testuser';
 function App() {
     return (
         <Router>
             <div className="App">
+                {/* Define the routes*/}
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/register" element={<Register/>}/>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
+                    <Route path="/testuser" element={<TestUser/>}/>
+                    <Route path="/testuserdashboard" element={<Testdashboard/>}/>
                 </Routes>
             </div>
         </Router>
